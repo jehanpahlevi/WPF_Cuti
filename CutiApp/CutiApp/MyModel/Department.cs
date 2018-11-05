4176f1843ce94fb4a7b1f1e89b132e9878463b72
Jehan Pahlevi <jehanmoch@gmail.com>
2018-11-05 08:37:41 +0700

    WPF_Cuti

---
+++ CutiApp/CutiApp/MyModel/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutiApp.MyModel
+{
+    public class Department : BaseModel
+    {
+        string Nama { get; set; }
+        string ManagerId { get; set; }
+    }
+}
---
+++ CutiApp/CutiApp/MyModel/Karyawan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutiApp.MyModel
+{
+    public class Karyawan : BaseModel
+    { 
+        public string Nama { get; set; }
+        public string Email { get; set; }
+        public string NoTelp { get; set; }
+        public string JobTitle { get; set; }
+        public string Status { get; set; }
+        public int JumlahAnak { get; set; }
+        public string Password { get; set; }
+        public int ThisYearBalance { get; set; }
+        public int LastYearBalance { get; set; }
+    }
+}
